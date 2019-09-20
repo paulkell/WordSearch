30ea0e64e7f0503a77bde30c8331ef1993ff7ad9
paulkell <tonykelley01@yahoo.com>
2019-09-19 20:10:01 -0400

    Add files via upload

---
+++ WordSearch/WordSearch/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xaml;
+using WordSearch.DictionaryFiles;
+
+namespace WordSearch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static List<string> resultsList = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void Compute(object sener, RoutedEventArgs args)
+        {
+            List<char> inputList = new List<char>();
+
+            new DictionaryReader();        
+            foreach(char character in ResultsTextBox.Text)
+            {
+                if (character >= 'a' && character <= 'z')
+                {
+                    inputList.Add(character);
+                }
+            }
+            new Node(inputList);
+            ResultsTextBox.Clear();
+            ResultsTextBox.Text = String.Join(Environment.NewLine, resultsList);
+        }
+    }
+}
---
+++ WordSearch/WordSearch/WordSearch.cs
@@ -0,0 +1,99 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Threading.Tasks;
+
+//namespace WordSearchCS
+//{
+//    class WordSearch
+//    {
+//        public static String[] DictionaryList;
+
+//        static void Main(string[] args)
+//        {
+
+//            List<char> LetterList = new List<char>();
+//            String FileName = "dictionary.txt";
+
+//            var t = Task.Run(() => GetInput(LetterList));
+//            var s = Task.Run(() => ReadDictionary(FileName));
+
+//            t.Wait();
+//            new Node(LetterList);
+
+//            Console.WriteLine("Press any key to end program...");
+//            Console.ReadKey();
+//        }
+
+//        public static void GetInput(List<char> LetterList)
+//        {
+//            char input = ' ';
+//            Console.Write("Enter characters: ");
+//            do
+//            {
+//                input = (char)Console.Read();
+//                if (input >= 'a' && input <= 'z')
+//                {
+//                    LetterList.Add(input);
+//                }
+//            } while (input >= 'a' && input <= 'z');
+//        }
+
+//        public static void ReadDictionary(String FileName)
+//        {
+//            DictionaryList = File.ReadAllLines(FileName);
+//        }
+
+//        public static bool DictionarySearch(String Word)
+//        {
+//            return Array.BinarySearch(DictionaryList, Word) > 0;
+//        }
+//    }
+
+//class Node
+//{
+//    public List<Node> children = new List<Node>();
+//    public char data;
+//    public String Word = "";
+
+//    public Node(List<char> LetterList)
+//    {
+//        CreateLevel(LetterList);
+//    }
+
+//    private Node(char letter, List<char> LetterList, String ParentWord)
+//    {
+//        data = letter;
+//        Word = ParentWord + data;
+//        if (WordSearch.DictionarySearch(Word))
+//        {
+//            Console.WriteLine(Word);
+//        }
+//        CreateLevel(LetterList);
+//    }
+
+//    private void CreateLevel(List<char> LetterList)
+//    {
+//        if (LetterList.Count > 0)
+//        {
+//            List<char> TemporaryLetterList;
+
+//            for (int i = 0; i < LetterList.Count; i++)
+//            {
+//                TemporaryLetterList = new List<char>(LetterList.ToArray());
+//                TemporaryLetterList.RemoveAt(i);
+//                children.Add(new Node(LetterList[i], TemporaryLetterList, Word));
+//            }
+//        }
+//    }
+
+//    public void ShowChildren()
+//    {
+//        foreach (Node child in children)
+//        {
+//            Console.Write(child.data + " ");
+//        }
+//        Console.WriteLine();
+//    }
+//}
+//}
