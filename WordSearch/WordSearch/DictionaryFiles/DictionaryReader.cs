9f121ff93f96bd564d3218bea0bb2e9ffb744aba
paulkell <tonykelley01@yahoo.com>
2019-09-21 17:45:32 -0400

    Added new function to add words to dictionary

--- WordSearch/WordSearch/DictionaryFiles/DictionaryReader.cs
+++ WordSearch/WordSearch/DictionaryFiles/DictionaryReader.cs
@@ -14,20 +14,77 @@ namespace WordSearch.DictionaryFiles
 
         public DictionaryReader()
         {
-            ReadDictionary(DictionaryName);
+            ReadDictionaryFromFile(DictionaryName);
         }
-        private static void ReadDictionary(String DictionaryName)
+
+        /// <summary>
+        /// Initializes internal dictionary from input file
+        /// </summary>
+        /// <param name="DictionaryName">The dictionary file to be read</param>
+        private static void ReadDictionaryFromFile(String DictionaryName)
         {
             DictionaryList = File.ReadAllLines(DictionaryName);
         }
 
+        /// <summary>
+        /// Searches dictionary for input word
+        /// </summary>
+        /// <param name="Word">The word to be searched for in dictionary</param>
+        /// <returns></returns>
         public static bool DictionarySearch(String Word)
         {
             return Array.BinarySearch(DictionaryList, Word) > 0;
         }
+
+        /// <summary>
+        /// Removes all words in input list from dictionary
+        /// </summary>
+        /// <param name="removeList">A list of words to be removed from dictionary</param>
         public static void RemoveWords(List<string> removeList)
         {
             DictionaryList = DictionaryList.Where(word => !removeList.Contains(word)).ToArray();
+            OverwriteDictionaryFile();
+        }
+
+        /// <summary>
+        /// Adds all words in input list to dictionary
+        /// </summary>
+        /// <param name="addList">A list of words to be added to dictionary</param>
+        public static void AddWords(List<string> addList)
+        {
+            int locationIndex;
+
+            foreach (String Word in addList)
+            {
+                locationIndex = Array.BinarySearch(DictionaryList, Word);
+                if(locationIndex >= 0)
+                {
+                    //DictionaryList already contains this word
+                    continue;
+                }
+                else
+                {
+                    locationIndex = ~locationIndex;
+                    string[] beforeIndex = new string[locationIndex];
+                    string[] afterIndex = new string[DictionaryList.Length - locationIndex];
+
+                    Array.Copy(DictionaryList, 0, beforeIndex, 0, locationIndex);
+                    Array.Copy(DictionaryList, locationIndex, afterIndex, 0, DictionaryList.Length - locationIndex);
+
+                    List<string> temp = beforeIndex.Append(Word).ToList();
+                    temp.AddRange(afterIndex.ToList());
+                    DictionaryList = temp.ToArray();
+                }
+            }
+
+            OverwriteDictionaryFile();
+        }
+
+        /// <summary>
+        /// Overwrites dictionary file with internal dictionary
+        /// </summary>
+        private static void OverwriteDictionaryFile()
+        {
             File.WriteAllLines(DictionaryName, DictionaryList);
         }
     }
